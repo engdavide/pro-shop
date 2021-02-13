b88326aa209720ff66726a0ae207b788adadcbb1
david.eng <twodoorbells@gmail.com>
2021-02-13 11:50:52 -0500

    init mongo db connection

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 
 import products from './products.js'
+import connectDb from './config/connectDB.js'
+
+dotenv.config()
+connectDb()
 
 const app = express()
 
@@ -17,4 +22,4 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000
 const MODE = process.env.NODE_ENV
 
-app.listen(PORT, console.log(`sever up on ${PORT} in ${MODE} mode pew pew`))
+app.listen(PORT, console.log(`sever up on ${PORT} in ${MODE} mode pew pew`.yellow.bold))
