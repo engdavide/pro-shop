bb2d112071469e349ace16c1bfd9593a7ef2a067
david.eng <twodoorbells@gmail.com>
2021-02-13 11:08:10 -0500

    tie-in api to client as component state

--- client/src/layouts/HomeLayout.js
+++ client/src/layouts/HomeLayout.js
@@ -1,8 +1,24 @@
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { Product } from '../components/lib'
 
+//Will be redux later...
+import axios from 'axios'
+
+
+
 export const HomeLayout = () => {
+	
+	//Will be redux later...
+	const [products, setProducts] = useState([])
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get('/api/products')
+			setProducts(data)
+		}
+		fetchProducts()
+	}, [])
+	
 	return(
 		<>
 			<h1>Products</h1>
--- client/src/layouts/ProductLayout.js
+++ client/src/layouts/ProductLayout.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import { Rating } from '../components/lib'
 
+import axios from 'axios'
 
-//Eventual backend...
-import products from '../products'
+
 
 
 export const ProductLayout = ( { match }) => {
 	//eventual backend...
-	const product = products.find(p => p._id === match.params.id)
+		//Will be redux later...
+	const [product, setProduct] = useState([])
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${match.params.id}`)
+			setProduct(data)
+		}
+		fetchProduct()
+	}, [])
 	
 	return(
 		<>
