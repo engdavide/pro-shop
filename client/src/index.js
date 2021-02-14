360df425f80c1a2cbe1f149c1e2c4dc5bae54e2a
david.eng <twodoorbells@gmail.com>
2021-02-14 08:42:34 -0500

    setup redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import store from './store'
 import './index.css';
 import './bootstrap.min.css'
   //Using bootswatch Sandstone
@@ -7,9 +9,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
