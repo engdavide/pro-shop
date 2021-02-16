b0007dfeb81d1c2471ea4db8b29ab767cf01c7f2
engdavide <twodoorbells@gmail.com>
2021-02-16 13:03:13 +0100

    add login and logout actions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,65 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, NavDropdown, Nav } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
-export function Header(){
-	return (
-		<header>
-			<Navbar bg="dark" variant='dark' collapseOnSelect expand="lg">
-					<Container>
-					<LinkContainer to='/'>
-						<Navbar.Brand>Trading Co.</Navbar.Brand>
-					</LinkContainer>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="ml-auto">
-							<LinkContainer to='/cart'>
-								<Nav.Link><i className='fas fa-shopping-cart'> Cart</i></Nav.Link>
-							</LinkContainer>
-							<LinkContainer to='/login'>
-								<Nav.Link><i className='fas fa-sign-in-alt'> Login</i></Nav.Link>
-							</LinkContainer>
-							<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-								<NavDropdown.Divider />
-								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-							</NavDropdown>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-		</header>
-	)
+export function Header() {
+  const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' collapseOnSelect expand='lg'>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Trading Co.</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'> Cart</i>
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <Nav.Link>
+                    <i className='fas fa-sign-in-alt'> Login</i>
+                  </Nav.Link>
+                )}
+              </LinkContainer>
+              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.2'>
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action/3.4'>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
